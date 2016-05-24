cb3ba8c0a9ff99db39e491e765e209b5f7d51d2a
spirraw <jankristianherring@gmail.com>
2016-05-24 19:20:23 +0300

    added pages

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,31 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function login() {
+        View::make('util/login.html');
+    }
+    
+    public static function owned_list() {
+        View::make('own/owned_pokemon_list.html');
+    }
+
+    public static function owned_pokemon() {
+        View::make('own/owned_pokemon.html');
+    }
+
+    public static function owned_edit() {
+        View::make('own/owned_pokemon_edit.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,19 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
+  
+    $routes->get('/owned', function() {
+    HelloWorldController::owned_list();
+  });
+  
+    $routes->get('/owned/pokemon', function() {
+    HelloWorldController::owned_pokemon();
+  });
+  
+    $routes->get('/owned/edit', function() {
+    HelloWorldController::owned_edit();
+  });
