b652d5684003e217c3b0d456d7b21f0a27d3a3e2
spirraw <jankristianherring@gmail.com>
2016-06-09 17:22:07 +0300

    clean pokemon type

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,7 +4,7 @@ class HelloWorldController extends BaseController {
 
     public static function index() {
         // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-        echo 'Tämä on etusivu!';
+        Redirect::to('/login');
     }
 
     public static function sandbox() {
